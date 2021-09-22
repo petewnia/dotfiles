9bc0370a0a268c388cc6970520b2fd88dff67847
Tew <onetiwthree@pm.me>
2021-09-22 23:12:07 +1200

    Update things

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,13 +1,21 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
 
+
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
-	vis:command('set theme pye')
+	vis:command('set theme meimu')
+	vis:command('set autoindent')
+	vis:command('set tabwidth 4')
+	vis:command('set expandtab')
+	vis:command('set ignorecase')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	-- Your per window configuration options e.g.
 	-- vis:command('set number')
+	vis:command('set show-tabs')
+	vis:command('set show-spaces')
+	vis:command('set show-newlines')
 end)
 
